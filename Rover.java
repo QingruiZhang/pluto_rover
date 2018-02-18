0b60bec3b6484a7cb9233f665a0fee4ead1ae287
QingruiZhang <32812278+QingruiZhang@users.noreply.github.com>
2018-02-18 10:49:27 +0000

    Initial commit

---
+++ Rover.java
@@ -0,0 +1,25 @@
+package pluto_rover;
+
+enum Direction {
+	N, S, W, E
+}
+
+public class Rover {
+	
+	int x;
+	int y;
+	Direction d;
+	
+	public Rover(int x, int y, Direction d) {
+		
+	}
+	
+	public void move(String commands) {
+		
+	}
+	
+	private void move(char command) {
+		
+	}
+
+}
---
+++ RoverTest.java
@@ -0,0 +1,14 @@
+package pluto_rover;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class RoverTest {
+	
+	@Test
+	public void moveForwardOnce() {
+		Rover rover = new Rover(0,0,Direction.N);
+		rover.move("F");
+		assertEquals("move forward once", rover.y, 1);
+	}
+}
